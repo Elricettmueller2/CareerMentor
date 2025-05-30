57e21171094a72f911f0f53e4e96112c6c0f4968
Elric Ettmüller <elric.ettmueller@hm.edu>
2025-05-30 10:21:18 +0200

    Feat: Mirroed Architecture Demo and setup basic frontend (Expo) and backend (FastAPI)  + Dockerisation

---
+++ Agentic_AI_System/backend-api/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Query
+from pydantic import BaseModel
+
+# Create a FastAPI app
+app = FastAPI()
+
+# Pydantic model for POST body
+class CommandRequest(BaseModel):
+    command: str
+@app.get("/healthcheck", tags=["System"])
+def is_healthy(test: str = Query("test", description="Enter any string as test parameter")):
+    return {"message": f"Successfully extracted URL param from GET request: {test}."}
+
+
+@app.post("/run", tags=["Application"])
+def run_command(request: CommandRequest):
+    command = request.command
+    if command == "log":
+        print("Logging to terminal...")
+        return {"message": "Printing to terminal."}
+    elif command == "execute":
+        return {"message": "Starting the application."}
+    else:
+        return {"message": f"Unknown command: {command}!"}
